44c5262d583edfba7d46902af9243353b0b11083
Myron William Echenim <myron.echenim@live.com>
2019-02-09 01:22:36 +0100

    update bubble sort implementation

--- Sorting-BubbleSort/Program.cs
+++ Sorting-BubbleSort/Program.cs
@@ -2,11 +2,48 @@
 
 namespace Sorting_BubbleSort
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] unSortedDate = { 20, 35, -15, -15, 7, 55, 1, -22 };
+            var first = $"[{string.Join(", ", unSortedDate)}]";
+            Console.WriteLine("unsorted      :   " + first);
+            for (int lastUnSortedIndex = unSortedDate.Length - 1;
+                lastUnSortedIndex > 0;
+                lastUnSortedIndex--)
+            {
+                for (int i = 0; i < lastUnSortedIndex; i++)
+                {
+                    if (unSortedDate[i] > unSortedDate[i + 1])
+                    {
+                        Swap(unSortedDate, i, i + 1);
+                    }
+                }
+            }
+
+            var sortedData = unSortedDate;
+            var sorted = $"[{string.Join(", ", sortedData)}]";
+            Console.WriteLine("sorted     :   " + sorted);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// implement a swap function
+        /// </summary>
+        /// <param name="arrays"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        private static void Swap(int[] data, int firstIndex, int secondIndex)
+        {
+            if (firstIndex == secondIndex)
+            {
+                return;
+            }
+
+            var temperalStorage = data[firstIndex];
+            data[firstIndex] = data[secondIndex];
+            data[secondIndex] = temperalStorage;
         }
     }
 }
